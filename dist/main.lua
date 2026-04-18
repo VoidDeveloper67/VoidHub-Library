d45da9ffa29c07479dfdaade2fa1fc164abe765e
VoidDeveloper67 <juniortype123@gmail.com>
2026-04-18 23:15:00 +0100

    Update main.lua

--- dist/main.lua
+++ dist/main.lua
@@ -6,7 +6,7 @@ local Players     = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local RunService  = game:GetService("RunService")
 
-local BASE = "https://raw.githubusercontent.com/vonplayz_real/VoidHub/refs/heads/main/"
+local BASE = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Library/refs/heads/main/"
 local function load(path) return loadstring(game:HttpGet(BASE .. path))() end
 local function loadUrl(url) return loadstring(game:HttpGet(url))() end
 
